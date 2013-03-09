feb0f08e89f88597208739d748f5d568410d6707
DAvid Nuon <davidnuongm@gmail.com>
2013-03-09 15:13:38 -0800

    Can now include markdown files in a page

---
+++ guide.php
@@ -0,0 +1,13 @@
+<?php
+    // Variable Title
+    $title = "Guide";
+?>
+<?php include("inc/top.php"); ?>
+
+<div class="grid_">
+    <br>
+    <?php render_markdown(dirname(__FILE__) . "/workshop-copy/theguidetogit.md"); ?>
+</div>
+
+    
+<?php include("inc/bottom.php"); ?>
---
+++ inc/top.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<?php include("inc/func.php") ?>
+<html>
+	<head>
+            <title>
+                <?php 
+                    if(isset($title)) {
+                        echo "acm* workshop: " . $title;    
+                    } else {
+                        echo "acm* workshop";
+                    }
+                ?>
+            </title>
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/960.css">
+		<script src="js/live.js"></script>
+	</head>
+	<body>
+		<header>
+			<div id="heromast">
+				<h1>Git Workshop</h1>
+				<h2>Learn to manage your code</h2>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+                    <li><a href="guide.php">Guide</a></li>
+					<li><a href="drama.php">The Drama</a></li>
+					<li><a href="about.php">About</a></li>
+
+				</ul>
+			</nav>
+		</header>
+
+		<div class="body-content">
+			<div class="wrapper">
+			<article class="container_12">
