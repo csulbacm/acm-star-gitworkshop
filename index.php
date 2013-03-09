4191471c6d1c283be9fb22e986aeab53d379fefe
DAvid Nuon <davidnuongm@gmail.com>
2013-03-09 14:43:30 -0800

    Broke everything up to php template

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+    // Variable Title
+    $title = "This is a title";
+?>
+<?php include("inc/top.php"); ?>
+
+<p>
+    This is a test for all things in this world.
+    The lazy dog jump over the schitzophrenic
+    fox, which made coffee for all the animals
+    in the forest.
+</p>
+
+<p>
+    Then the fire nation attacked.
+</p>
+
+<ul>
+    <li>WOOOOOOOOOOOOOOOOOOOOO</li>
+    <li>WOOOOOOOOOOOOOOOOOOOOO</li>
+    <li>WOOOOOOOOOOOOOOOOOOOOO</li>
+    <li>WOOOOOOOOOOOOOOOOOOOOO</li>
+    <li>WOOOOOOOOOOOOOOOOOOOOO</li>
+    <li>WOOOOOOOOOOOOOOOOOOOOO</li>
+    <li>WOOOOOOOOOOOOOOOOOOOOO</li>
+    <li>WOOOOOOOOOOOOOOOOOOOOO</li>
+    <li>WOOOOOOOOOOOOOOOOOOOOO</li>
+    <li>WOOOOOOOOOOOOOOOOOOOOO</li>
+    <li>WOOOOOOOOOOOOOOOOOOOOO</li>
+    <li>WOOOOOOOOOOOOOOOOOOOOO</li>
+    <li>WOOOOOOOOOOOOOOOOOOOOO</li>
+    <li>WOOOOOOOOOOOOOOOOOOOOO</li>
+    <li>WOOOOOOOOOOOOOOOOOOOOO</li>
+    <li>WOOOOOOOOOOOOOOOOOOOOO</li>
+    <li>WOOOOOOOOOOOOOOOOOOOOO</li>
+    <li>WOOOOOOOOOOOOOOOOOOOOO</li>
+    <li>WOOOOOOOOOOOOOOOOOOOOO</li>
+</ul>
+
+<?php include("inc/bottom.php"); ?>
