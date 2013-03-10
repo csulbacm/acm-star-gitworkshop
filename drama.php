99c4f158484da02fe8c60d0ba863cfebdbad6547
David Nuon <david@lv-node64.buyvm.net>
2013-03-10 11:50:52 +0400

    Edited slidedeck

--- drama.php
+++ drama.php
@@ -18,54 +18,55 @@
 
   <body style='display: none'>
     <section class='slides layout-regular template-default'>   
-<article><img src="img/slides/Slide1.PNG" alt="1"/></article>
-<article><img src="img/slides/Slide2.PNG" alt="2"/></article>
-<article><img src="img/slides/Slide3.PNG" alt="3"/></article>
-<article><img src="img/slides/Slide4.PNG" alt="4"/></article>
-<article><img src="img/slides/Slide5.PNG" alt="5"/></article>
-<article><img src="img/slides/Slide6.PNG" alt="6"/></article>
-<article><img src="img/slides/Slide7.PNG" alt="7"/></article>
-<article><img src="img/slides/Slide8.PNG" alt="8"/></article>
-<article><img src="img/slides/Slide9.PNG" alt="9"/></article>
-<article><img src="img/slides/Slide10.PNG" alt="10"/></article>
-<article><img src="img/slides/Slide11.PNG" alt="11"/></article>
-<article><img src="img/slides/Slide12.PNG" alt="12"/></article>
-<article><img src="img/slides/Slide13.PNG" alt="13"/></article>
-<article><img src="img/slides/Slide14.PNG" alt="14"/></article>
-<article><img src="img/slides/Slide15.PNG" alt="15"/></article>
-<article><img src="img/slides/Slide16.PNG" alt="16"/></article>
-<article><img src="img/slides/Slide17.PNG" alt="17"/></article>
-<article><img src="img/slides/Slide18.PNG" alt="18"/></article>
-<article><img src="img/slides/Slide19.PNG" alt="19"/></article>
-<article><img src="img/slides/Slide20.PNG" alt="20"/></article>
-<article><img src="img/slides/Slide21.PNG" alt="21"/></article>
-<article><img src="img/slides/Slide22.PNG" alt="22"/></article>
-<article><img src="img/slides/Slide23.PNG" alt="23"/></article>
-<article><img src="img/slides/Slide24.PNG" alt="24"/></article>
-<article><img src="img/slides/Slide25.PNG" alt="25"/></article>
-<article><img src="img/slides/Slide26.PNG" alt="26"/></article>
-<article><img src="img/slides/Slide27.PNG" alt="27"/></article>
-<article><img src="img/slides/Slide28.PNG" alt="28"/></article>
-<article><img src="img/slides/Slide29.PNG" alt="29"/></article>
-<article><img src="img/slides/Slide30.PNG" alt="30"/></article>
-<article><img src="img/slides/Slide31.PNG" alt="31"/></article>
-<article><img src="img/slides/Slide32.PNG" alt="32"/></article>
-<article><img src="img/slides/Slide33.PNG" alt="33"/></article>
-<article><img src="img/slides/Slide34.PNG" alt="34"/></article>
-<article><img src="img/slides/Slide35.PNG" alt="35"/></article>
-<article><img src="img/slides/Slide36.PNG" alt="36"/></article>
-<article><img src="img/slides/Slide37.PNG" alt="37"/></article>
-<article><img src="img/slides/Slide38.PNG" alt="38"/></article>
-<article><img src="img/slides/Slide39.PNG" alt="39"/></article>
-<article><img src="img/slides/Slide40.PNG" alt="40"/></article>
-<article><img src="img/slides/Slide41.PNG" alt="41"/></article>
-<article><img src="img/slides/Slide42.PNG" alt="42"/></article>
-<article><img src="img/slides/Slide43.PNG" alt="43"/></article>
-<article><img src="img/slides/Slide44.PNG" alt="44"/></article>
-<article><img src="img/slides/Slide45.PNG" alt="45"/></article>
-<article><img src="img/slides/Slide46.PNG" alt="46"/></article>
-<article><img src="img/slides/Slide47.PNG" alt="47"/></article>
-<article><img src="img/slides/Slide48.PNG" alt="48"/></article>
+	<article><img src="img/slides/Slide1.PNG" alt="1"/></article>
+	<article><img src="img/slides/Slide2.PNG" alt="2"/></article>
+	<article><img src="img/slides/Slide3.PNG" alt="3"/></article>
+	<article><img src="img/slides/yolo.png"   alt="yolo"/></article>
+	<article><img src="img/slides/Slide4.PNG" alt="4"/></article>
+	<article><img src="img/slides/Slide5.PNG" alt="5"/></article>
+	<article><img src="img/slides/Slide6.PNG" alt="6"/></article>
+	<article><img src="img/slides/Slide7.PNG" alt="7"/></article>
+	<article><img src="img/slides/Slide8.PNG" alt="8"/></article>
+	<article><img src="img/slides/Slide9.PNG" alt="9"/></article>
+	<article><img src="img/slides/Slide10.PNG" alt="10"/></article>
+	<article><img src="img/slides/Slide11.PNG" alt="11"/></article>
+	<article><img src="img/slides/Slide12.PNG" alt="12"/></article>
+	<article><img src="img/slides/Slide13.PNG" alt="13"/></article>
+	<article><img src="img/slides/Slide14.PNG" alt="14"/></article>
+	<article><img src="img/slides/Slide15.PNG" alt="15"/></article>
+	<article><img src="img/slides/Slide16.PNG" alt="16"/></article>
+	<article><img src="img/slides/Slide17.PNG" alt="17"/></article>
+	<article><img src="img/slides/Slide18.PNG" alt="18"/></article>
+	<article><img src="img/slides/Slide19.PNG" alt="19"/></article>
+	<article><img src="img/slides/Slide20.PNG" alt="20"/></article>
+	<article><img src="img/slides/Slide21.PNG" alt="21"/></article>
+	<article><img src="img/slides/Slide22.PNG" alt="22"/></article>
+	<article><img src="img/slides/Slide23.PNG" alt="23"/></article>
+	<article><img src="img/slides/Slide24.PNG" alt="24"/></article>
+	<article><img src="img/slides/Slide25.PNG" alt="25"/></article>
+	<article><img src="img/slides/Slide26.PNG" alt="26"/></article>
+	<article><img src="img/slides/Slide27.PNG" alt="27"/></article>
+	<article><img src="img/slides/Slide28.PNG" alt="28"/></article>
+	<article><img src="img/slides/Slide29.PNG" alt="29"/></article>
+	<article><img src="img/slides/Slide30.PNG" alt="30"/></article>
+	<article><img src="img/slides/Slide31.PNG" alt="31"/></article>
+	<article><img src="img/slides/Slide32.PNG" alt="32"/></article>
+	<article><img src="img/slides/Slide33.PNG" alt="33"/></article>
+	<article><img src="img/slides/Slide34.PNG" alt="34"/></article>
+	<article><img src="img/slides/Slide35.PNG" alt="35"/></article>
+	<article><img src="img/slides/Slide36.PNG" alt="36"/></article>
+	<article><img src="img/slides/Slide37.PNG" alt="37"/></article>
+	<article><img src="img/slides/Slide38.PNG" alt="38"/></article>
+	<article><img src="img/slides/Slide39.PNG" alt="39"/></article>
+	<article><img src="img/slides/Slide40.PNG" alt="40"/></article>
+	<article><img src="img/slides/Slide41.PNG" alt="41"/></article>
+	<article><img src="img/slides/Slide42.PNG" alt="42"/></article>
+	<article><img src="img/slides/Slide43.PNG" alt="43"/></article>
+	<article><img src="img/slides/Slide44.PNG" alt="44"/></article>
+	<article><img src="img/slides/Slide45.PNG" alt="45"/></article>
+	<article><img src="img/slides/Slide46.PNG" alt="46"/></article>
+	<article><img src="img/slides/Slide47.PNG" alt="47"/></article>
+	<article><img src="img/slides/Slide48.PNG" alt="48"/></article>
     </section>
 
   </body>
