8dbabf67c2a7714e0b45acb5ee878b78529ff4a7
DAvid Nuon <davidnuongm@gmail.com>
2013-03-12 21:11:20 -0700

    Started quickstart guide

---
+++ quickstart.php
@@ -0,0 +1,20 @@
+<?php
+    // Variable Title
+    $title = "Quickstart";
+?>
+<?php include("inc/top.php"); ?>
+
+<div class="quick-guide">
+	<div class="grid_12">
+		<h1>Quick-Start Guide</h1>
+		<h2>Here's what you need to get started!</h2>
+	</div>
+
+	<div class="grid_12">
+		<h2>Download Git</h2>
+	</div>
+	<div class="grid_6 download"><a href="#">Download for Windows</a></div>
+	<div class="grid_6 download"><a href="#">Download for Windows</a></div>
+</div>
+    
+<?php include("inc/bottom.php"); ?>
