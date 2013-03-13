f2c23b542112fe6500ee9e594c0e40069da313c7
DAvid Nuon <davidnuongm@gmail.com>
2013-03-13 03:47:44 -0700

    Updated Get Started

--- quickstart.php
+++ quickstart.php
@@ -13,15 +13,35 @@
 	<div class="grid_12">
 		<h3>1. Sign up for GitHub</h3>
 	</div>
-	<div class="grid_12 download"><a href="#" target="_blank">Github.com</a></div>
+    <div class="grid_12 download">
+        <a href="http://www.github.com" target="_blank">
+            Github.com
+        </a>
+    </div>
 	
 
 	<div class="grid_12">
 		<h3>2. Download Git</h3>
 	</div>
-	<div class="grid_6 download"><a href="#">Download for Windows</a></div>
-	<div class="grid_6 download"><a href="#">Download for Mac</a></div>
 
+    <div class="grid_6 download">
+        <a href="http://windows.github.com/" target="_blank">
+            Download for Windows
+        </a>
+    </div>
+
+    <div class="grid_6 download">
+        <a href="http://mac.github.com/" target="_blank">
+            Download for Mac
+        </a>
+    </div>
+
+
+    <div class="grid_12 download">
+        <a href="http://eclipse.github.com/" style="color:#333;padding:2px;font-size:15px;background:none;"
+             target="_blank">
+            There's also an Eclipse plugin.
+        </a>
 
 </div>
     
