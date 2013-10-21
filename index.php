fe226274788eefa1ccfc1671b3d1ac7b37eec15f
David Nuon <davidnuon@dn-Rechter.(none)>
2013-10-20 22:13:33 -0700

    changed front page

--- index.php
+++ index.php
@@ -4,14 +4,29 @@
 ?>
 <?php include("inc/top.php"); ?>
 
-
-<div class="grid_6">
-	<br />
-	<br />
-	<img src="img/octocat.png" alt="Super Octocat" />
+<div class="grid_12">
+	<img src="img/octocat.png" alt="Octocat" style="margin:20px auto; display:block;">
 </div>
 
-<div class="grid_6">
-        <?php render_markdown(dirname(__FILE__) . "/workshop-copy/index.md"); ?>	
+<div class="grid_4 download call_to_action">
+	<h3>1. Sign Up</h3>
+	<p>Seats are limited, sign up while they last!</p>
+	<a href="./signup.php">Register Here</a>
 </div>
+
+<div class="grid_4 call_to_action download">
+	<h3>2. Get Prepared</h3>
+	<p>
+		Please review steps here for getting your machine setup.
+	</p>
+	<a href="./prereq.php">Requirements</a>
+</div>
+
+<div class="grid_4 call_to_action download">
+	<h3>3. About</h3>
+	
+	<p>This was brought to you by EAT, ACM and acm-sigweb.</p>
+	<a href="./about.php">Learn more</a>
+</div>
+
 <?php include("inc/bottom.php"); ?>
