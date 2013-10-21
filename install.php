7c7a3c821580291195db5831b924f7cd7ff0517d
Jake Schmitz <jake.d.schmitz@gmail.com>
2013-10-20 20:22:58 -0700

    pages ready for styling

--- install.php
+++ install.php
@@ -3,14 +3,13 @@
     $title = "Install";
 ?>
 <?php include("inc/top.php"); ?>
-
-<img style="text-align: center;" src="img/GitSetup1.png" />
-<img style="text-align: center;" src="img/GitSetup2.png" />
-<img style="text-align: center;" src="img/GitSetup3.png" />
-<img style="text-align: center;" src="img/GitSetup4.png" />
-<img style="text-align: center;" src="img/GitSetup5.png" />
-<img style="text-align: center;" src="img/GitSetup6.png" />
-<img style="text-align: center;" src="img/GitSetup7.png" />
-<img style="text-align: center;" src="img/GitSetup8.png" />
-<img style="text-align: center;" src="img/GitSetup9.png" />
+<img src="img/GitSetup1.png" />
+<img src="img/GitSetup2.png" />
+<img src="img/GitSetup3.png" />
+<img src="img/GitSetup4.png" />
+<img src="img/GitSetup5.png" />
+<img src="img/GitSetup6.png" />
+<img src="img/GitSetup7.png" />
+<img src="img/GitSetup8.png" />
+<img src="img/GitSetup9.png" />
 <?php include("inc/bottom.php"); ?>
