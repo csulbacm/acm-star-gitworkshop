4e10447991ed8c3afb32c2cbef6589086c71d250
Julia Lai <jwwlai@gmail.com>
2013-10-20 19:47:33 -0700

    Added general description in about.md

--- about.php
+++ about.php
@@ -6,9 +6,20 @@
 
 <div class="grid_12">
     <h2 style="font-size:45px;text-align:center">
+        About the Workshop
+    </h2>
+</div>
+
+<div class="grid_12">
+    <?php render_markdown(dirname(__FILE__) . '/workshop-copy/about.md'); ?>  
+</div>
+
+<div class="grid_12">
+    <h2 style="font-size:45px;text-align:center">
         Brought to you by
     </h2>
 </div>
+
 
 <div class="grid_6">
     <?php render_markdown(dirname(__FILE__) . '/workshop-copy/julia.md'); ?>  
@@ -18,5 +29,10 @@
     <?php render_markdown(dirname(__FILE__) . '/workshop-copy/david.md'); ?>  
 </div>
 
+<div class="clear"> </div>
+
+<div class="grid_6">
+    <?php render_markdown(dirname(__FILE__) . '/workshop-copy/steven.md'); ?>  
+</div>
 
 <?php include("inc/bottom.php"); ?>
